8710dc975b8dc1e6e5f4d1ff2bbe4d30707495fe
THARUN KUMAR <tharunkumar7041@gmail.com>
2025-12-15 00:34:53 +0530

    feat: Implement fan speed control for HP Omen, including maximum and normal settings

---
+++ SetNormalFan.ps1
@@ -0,0 +1,53 @@
+# Set HP Omen Fan Speed to Normal (Auto)
+
+# Start a CIM session
+$Session = New-CimSession -Name 'hpq' -SkipTestConnection
+
+# Function to send BIOS WMI commands
+Function Send-OmenBiosWmi {
+    param (
+         [ValidateNotNullOrEmpty()] [UInt32] $Command = 0x20008,
+         [Parameter(Mandatory)] [UInt32] $CommandType,
+         [Byte[]] $Data = $Null,
+         [ValidateNotNullOrEmpty()] [Byte[]] $Sign = @(0x53, 0x45, 0x43, 0x55),
+         [ValidateSet('0', '4', '128', '1024', '4096')] [String] $OutputSize = '0'
+    )
+
+    # Prepare the request
+    If($Data -eq $Null) {
+        $BiosDataIn = New-CimInstance -ClassName 'hpqBDataIn' -ClientOnly -Namespace 'root\wmi' -Property @{
+            Command = $Command;
+            CommandType = $CommandType;
+            Size = [UInt32] 0;
+            Sign = $Sign;
+        }
+    } Else {
+        $BiosDataIn = New-CimInstance -ClassName 'hpqBDataIn' -ClientOnly -Namespace 'root\wmi' -Property @{
+            Command = $Command;
+            CommandType = $CommandType;
+            hpqBData = $Data;
+            Size = [UInt32] $Data.Length;
+            Sign = $Sign;
+        }
+    }
+
+    # Get BIOS method class instance
+    $BiosMethods = Get-CimInstance -ClassName 'hpqBIntM' -CimSession $Session -Namespace 'root\wmi'
+
+    # Call the BIOS method
+    $Result = Invoke-CimMethod -InputObject $BiosMethods -MethodName ('hpqBIOSInt' + $OutputSize) -Arguments @{InData = [CimInstance] $BiosDataIn}
+
+    # Check result
+    If($Result. OutData.rwReturnCode -eq 0) {
+        Write-Host '✓ Fan speed set to normal (auto) successfully!' -ForegroundColor Green
+    } Else {
+        Write-Host "✗ Failed:  Error $($Result.OutData. rwReturnCode)" -ForegroundColor Red
+    }
+}
+
+# Set Maximum Fan Speed Off (returns to automatic control)
+Write-Host 'Setting fan speed to normal (automatic control)...' -ForegroundColor Cyan
+Send-OmenBiosWmi -CommandType 0x27 -Data 0x00
+
+# Terminate the CIM session
+Remove-CimSession -CimSession $Session
